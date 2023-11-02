2afc15038b5145198c0c3ba67d312c278f2d5aec
Duy Quoc Anh Nguyen <84012972+AnhNotAnh@users.noreply.github.com>
2023-11-02 11:58:36 +1030

    Create card for region country in country tempurature

--- Assig2/a2reactapp/src/components/CountryTemperatureDetail.js
+++ Assig2/a2reactapp/src/components/CountryTemperatureDetail.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useLocation } from "react-router-dom";
 
 
 
@@ -10,6 +10,7 @@ function CountryTemperatureDetail() {
     const [maxYear, setMaxYear] = useState([]);
     const [countryId, setCountryId] = useState(params.countryId)
     const [regionId, setRegionId] = useState(params.regionId)
+    const countryData = useLocation();
     
 
     useEffect(() => {
@@ -29,6 +30,17 @@ function CountryTemperatureDetail() {
                 <h2>Country Temperature Data From {maxYear} to {minYear}</h2>
             </div>
             <div className="container text-center">
+                <div class="row justify-content-center mt-3">
+                    <div className="col-3">
+                        <div className="card mb-2" style={{ width: 18 + 'rem' }} >
+                            <img className="card-img-top" src={countryData.state.countryImage} alt={"Image of " + countryData.state.countryName} />
+                            <div className="card-body">
+                                <h5 className="card-title">Region Name: {countryData.state.regionName}</h5>
+                                <h5 className="card-title">Country Name: {countryData.state.countryName}</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div class="row justify-content-center mt-3">
                     <div class="col">
                         <table class="table table-bordered">
