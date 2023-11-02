1d9b9e8fe7f4e69934fc0cb88e68a29b9970de21
Duy Quoc Anh Nguyen <84012972+AnhNotAnh@users.noreply.github.com>
2023-11-03 10:08:34 +1030

    Setting up table for country emission summary
    Create table to show a summary of emission,
    Back button to country

--- Assig2/a2reactapp/src/components/CountryEmissionDetail.js
+++ Assig2/a2reactapp/src/components/CountryEmissionDetail.js
@@ -21,6 +21,30 @@ function CountryEmissionDetail() {
                         </div>
                     </div>
                 </div>
+                <div class="row justify-content-center mt-3">
+                    <div class="col">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Year</th>
+                                    <th scope="col">Element</th>
+                                    <th scope="col">Total Value</th>
+                                    
+                                </tr>
+                            </thead>
+                            {countryTemData.map((obj) => (
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">{obj.theCountryTempData.year}</th>
+                                        <td>{obj.theCountryTempData.unit} </td>
+                                        <td>{obj.theCountryTempData.change} </td>
+                                    </tr>
+                                </tbody>))
+                            }
+                        </table>
+                    </div>
+                </div>
+                <Link to={"/Country/" + countryData.state.regionId} className="btn btn-primary mb-2">Back to Country</Link>
             </div>
 
 
