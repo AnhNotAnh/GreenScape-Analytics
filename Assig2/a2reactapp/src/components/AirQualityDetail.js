f5930b55f91ce471ebb31fffd516ebd1862b7ec6
Duy Quoc Anh Nguyen <84012972+AnhNotAnh@users.noreply.github.com>
2023-11-05 12:13:44 +1030

    Create city card to display country, region

--- Assig2/a2reactapp/src/components/AirQualityDetail.js
+++ Assig2/a2reactapp/src/components/AirQualityDetail.js
@@ -1,10 +1,31 @@
+import React, { useState, useEffect } from 'react'
+import { useParams, Link, useLocation } from "react-router-dom";
 function AirQualityDetail() {
+
+    const cityData = useLocation();
 
     return (
         <>
             <div>
                 <h2>Air Quality Detail</h2>
             </div>
+            <div className="container text-center">
+                <div className="row justify-content-center mt-3">
+                    <div className="col-3">
+                        <div className="card mb-2" style={{ width: 18 + 'rem' }} >
+                            <img className="card-img-top" src={cityData.state.countryImage} alt={"Image of " + cityData.state.countryName} />
+                            <div className="card-body">
+                                <p className="card-title">Region name: {cityData.state.regionName}</p>
+                                <p className="card-title">Country name: {cityData.state.countryName}</p>
+                                {/*<p className="card-title">City name: {cityData.state.regionName}</p>*/}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+
+
         </>
     )
 }
